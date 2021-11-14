7a3a55c2cc1773edf5095a4143d8650912c3d16a
WDEXcourse <wdkawasaki.online@gmail.com>
2021-11-14 11:35:56 +0900

    2段ジャンプ

---
+++ Assets/JokerAction.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JokerAction : MonoBehaviour
+{
+    private float pushTime;
+    private Rigidbody rb;
+    [SerializeField]
+    int JumpCount = 0;
+    [SerializeField]
+    private float MoveSpeed = 0;
+    [SerializeField]
+    private float JumpPower = 0;
+    [SerializeField]
+    private float GravityPower = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            transform.position += new Vector3(-10 * MoveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.position += new Vector3(10 * MoveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pushTime+=Time.deltaTime;
+        }
+        else if (JumpCount <= 1 && Input.GetKeyUp(KeyCode.W))
+        {
+            Debug.Log(pushTime);
+            if (pushTime <= 0.1f)
+            {
+                rb.AddForce(Vector3.up * JumpPower * 0.5f, ForceMode.Impulse);
+                Debug.Log("小ジャンプ");
+            }
+            else
+            {
+                rb.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+                Debug.Log("大ジャンプ");
+                pushTime = 0;
+            }
+            JumpCount++;
+            pushTime = 0;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector3.down * 9.81f * GravityPower,ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            JumpCount = 0;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            JumpCount = 1;
+        }
+    }
+}

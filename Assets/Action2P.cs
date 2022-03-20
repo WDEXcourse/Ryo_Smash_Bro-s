5e414ccc8a72c31062fcf94eca3d753c08d88f99
WDEXcourse <wdkawasaki.online@gmail.com>
2022-03-20 11:18:40 +0900

    2P追加

--- Assets/Action2P.cs
+++ Assets/Action2P.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JokerAction : MonoBehaviour
+public class Action2P : MonoBehaviour
 {
     public Vector3 startPos;
     private float pushTime;
@@ -42,6 +42,7 @@ public class JokerAction : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         transform.position = startPos;
+        transform.rotation = Quaternion.Euler(0, -90, 0);
         HitJudgement.SetActive(false);
     }
 
@@ -50,29 +51,29 @@ public class JokerAction : MonoBehaviour
     {
         if (MoveStop == false)
         {
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.K))
             {
                 downInput = true;
             }
 
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.J))
             {
                 transform.rotation = Quaternion.Euler(0, -90, 0);
                 transform.position += new Vector3(-10 * MoveSpeed * Time.deltaTime, 0, 0);
                 anim.SetBool("isRunning", true);
             }
-            else if (Input.GetKeyUp(KeyCode.A))
+            else if (Input.GetKeyUp(KeyCode.J))
             {
                 anim.SetBool("isRunning", false);
             }
 
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.L))
             {
                 transform.rotation = Quaternion.Euler(0, 90, 0);
                 transform.position += new Vector3(10 * MoveSpeed * Time.deltaTime, 0, 0);
                 anim.SetBool("isRunning", true);
             }
-            else if (Input.GetKeyUp(KeyCode.D))
+            else if (Input.GetKeyUp(KeyCode.L))
             {
                 anim.SetBool("isRunning", false);
             }
@@ -87,7 +88,7 @@ public class JokerAction : MonoBehaviour
             anim.SetBool("Attack", false);
         }
 
-        if (Input.GetKey(KeyCode.X))
+        if (Input.GetKey(KeyCode.M))
         {
             anim.SetBool("Knife1", true);
         }
@@ -96,11 +97,11 @@ public class JokerAction : MonoBehaviour
             anim.SetBool("Knife1", false);
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.I))
         {
             pushTime += Time.deltaTime;
         }
-        else if (JumpCount < 1 && Input.GetKeyUp(KeyCode.W))
+        else if (JumpCount < 1 && Input.GetKeyUp(KeyCode.I))
         {
             Debug.Log(pushTime);
             if (pushTime <= 0.1f)
@@ -118,7 +119,7 @@ public class JokerAction : MonoBehaviour
             pushTime = 0;
         }
 
-        if (JumpCount == 1 && Input.GetKeyDown(KeyCode.W))
+        if (JumpCount == 1 && Input.GetKeyDown(KeyCode.I))
         {
             rb.AddForce(Vector3.up * AirJumpPower, ForceMode.Impulse);
             Debug.Log("空中ジャンプ");
